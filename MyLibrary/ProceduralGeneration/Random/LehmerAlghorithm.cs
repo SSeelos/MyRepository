491e53f415e623f6919fdce3a248a116e009faff
SSeelos <sebastian.seelos@gmx.de>
2021-05-29 13:53:04 +0200

    Update LehmerAlghorithm.cs

--- MyLibrary/ProceduralGeneration/Random/LehmerAlghorithm.cs
+++ MyLibrary/ProceduralGeneration/Random/LehmerAlghorithm.cs
@@ -2,9 +2,31 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MyLibrary.ProceduralGeneration.Random
+namespace MyLibrary
 {
-    class LehmerAlghorithm
+    public class LehmerAlghorithm
     {
+        private const int a = 16807;
+        private const int m = 2147483647;
+        private const int q = 127773;
+        private const int r = 2836;
+        private int seed;
+
+        public LehmerAlghorithm(int seed)
+        {
+            if (seed <= 0 || seed == int.MaxValue)
+                throw new Exception("Bad seed");
+            this.seed = seed;
+        }
+
+        public double Next()
+        {
+            int hi = seed / q;
+            int lo = seed % q;
+            seed = (a * lo) - (r * hi);
+            if (seed <= 0)
+                seed = seed + m;
+            return (seed * 1.0) / m;
+        }
     }
 }
