77ebe162df99282f808d4c80f70252186ee12e7e
SSeelos <sebastian.seelos@gmx.de>
2021-06-14 14:55:01 +0200

    Revert "Merge branch 'main' of https://github.com/SSeelos/MyRepository into main"
    This reverts commit 165194036472393defb2202819d7385a34031276, reversing
    changes made to 0f9c7515b67d3c67ff031494525f6316fec0c844.

--- MyWPF/MainWindow.xaml.cs
+++ MyWPF/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using MyWPF.MVVM;
+using System.Reflection;
+using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace MyWPF
 {
@@ -13,25 +17,71 @@ namespace MyWPF
             InitializeComponent();
         }
 
-
-
-        private void Basics_Click(object sender, RoutedEventArgs e)
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            new BasicsWindow()
-                .ShowDialog();
+
         }
 
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(MethodBase.GetCurrentMethod().Name);
+        }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.MyTextBoxReadOnly.Text += ((CheckBox)sender).Content;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var txt = ((CheckBox)sender).Content.ToString();
+            var str = new StringBuilder(this.MyTextBoxReadOnly.Text);
+            str.Replace(txt, "");
+
+            this.MyTextBoxReadOnly.Text = str.ToString();
+        }
+
+        private void MyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var box = (ComboBox)sender;
+            var value = (ComboBoxItem)box.SelectedValue;
+
+            this.Log.Text = value.Content.ToString();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(MethodBase.GetCurrentMethod().Name);
+        }
+
+        private void MyTextBlock_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.Log.Text = this.MyTextBlock.Text;
+        }
+
+        private void OpenPage_Click(object sender, RoutedEventArgs e)
+        {
+            var page = new MyPage();
+
+            var navWindow = new NavigationWindow();
+            navWindow.Show();
+            navWindow.Navigate(page);
+        }
+
+        private void OpenWindow_Click(object sender, RoutedEventArgs e)
+        {
+            new MyWindow()
+                .Show();
+        }
 
         private void MVVM_Click(object sender, RoutedEventArgs e)
         {
             new MyMVVMWindow()
-                .ShowDialog();
-        }
-
-        private void UseLibrary_Click(object sender, RoutedEventArgs e)
-        {
-            new UseLibraryWindow()
                 .ShowDialog();
         }
     }
