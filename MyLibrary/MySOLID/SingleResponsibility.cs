54d9fc4b8200d839c86a19f1f59ce6355f1a123b
SSeelos <sebastian.seelos@gmx.de>
2021-08-03 20:22:14 +0200

    Solid all

--- MyLibrary/MySOLID/SingleResponsibility.cs
+++ MyLibrary/MySOLID/SingleResponsibility.cs
@@ -4,31 +4,130 @@ using System.Reflection;
 
 namespace MyLibrary
 {
-    public interface IShape { }
-    public class Square : IShape
+    //Single Responsibility Principle (S-RP)
+    //modules should have only one reason to change
+    //-> separate concerns
+
+    //Open closed Principle (O-CP)
+    //module/class is open for extension and closed for modification
+    //-> introduce interfaces (or abstract class)
+
+    //Liskov Substitution Principle (L-SP)
+    //be able to use any derived class instead of a parent class
+
+    //Interface Segregation Principle (ISP)
+    //clients should not be forced to implement interfaces they don't use
+    //-> many small interfaces based on groups of methods
+
+    public interface IShape1D
+    {
+        double DimX();
+    }
+    public interface IShape2D : IShape1D        //(ISP)
+    {
+        double Area();
+    }
+    public interface IShape3D : IShape2D        //(ISP)
+    {
+        double Volume();
+    }
+    public class Line : IShape1D
     {
         public double Lenght;
 
-        public Square(double lenght)
+        public Line(double lenght)
         {
             this.Lenght = lenght;
         }
+
+        public double DimX()
+        {
+            return this.Lenght;
+        }
+    }
+    public class Square : IShape2D
+    {
+        public double Length;
+
+        public Square(double lenght)
+        {
+            this.Length = lenght;
+        }
+
+        public double DimX()
+        {
+            return this.Length;
+        }
+        public double Area()
+        {
+            return Math.Pow(this.Length, 2);
+        }
+
     }
 
-    public class Circle : IShape
+    public class Circle : IShape2D
     {
         public double Radius;
         public Circle(double radius)
         {
             this.Radius = radius;
         }
+
+        public double DimX()
+        {
+            return this.Radius * 2;
+        }
+        public double Area()
+        {
+            return Math.PI * Math.Pow(this.Radius, 2);
+        }
+    }
+    public class HollowCircle : Circle
+    {
+        public double InnerRadius;
+        public HollowCircle(double radius, double innerRadius)
+            : base(radius)
+        {
+            this.InnerRadius = innerRadius;
+        }
+
+        public new double Area()      //(LSP)
+        {
+            var outerArea = Math.PI * Math.Pow(this.Radius, 2);
+            var innerArea = Math.PI * Math.Pow(this.InnerRadius, 2);
+            return outerArea - innerArea;
+        }
     }
 
-    public class AreaCalculator
+    public class Cube : IShape3D
     {
-        protected IShape[] shapes;
+        public double Length;
 
-        public AreaCalculator(params IShape[] shapes)
+        public Cube(double length)
+        {
+            this.Length = length;
+        }
+        public double Area()
+        {
+            return Math.Pow(this.Length, 2);
+        }
+
+        public double DimX()
+        {
+            return this.Length;
+        }
+
+        public double Volume()
+        {
+            return Math.Pow(this.Length, 3);
+        }
+    }
+
+    public class AreaCalculator             //(SRP)
+    {
+        protected IShape2D[] shapes;
+
+        public AreaCalculator(params IShape2D[] shapes)
         {
             this.shapes = shapes;
         }
@@ -38,19 +137,7 @@ namespace MyLibrary
             var area = new List<double>();
             foreach (var shape in shapes)
             {
-                switch (shape)
-                {
-                    case Square square:
-                        area.Add(Math.Pow(square.Lenght, 2));
-                        break;
-                    case Circle circle:
-                        area.Add(Math.PI * Math.Pow(circle.Radius, 2));
-                        break;
-
-                    default:
-                        break;
-
-                }
+                area.Add(shape.Area());     //(OCP) removes the need for a switch statement, that would have to be extendet
             }
             return area;
 
@@ -69,7 +156,7 @@ namespace MyLibrary
         }
     }
 
-    public class AreaCalculatorOutputter
+    public class AreaCalculatorOutputter    //(SRP)
     {
         protected AreaCalculator areaCalculator;
         public AreaCalculatorOutputter(AreaCalculator areaCalculator)
--- MyLibrary/MySOLID/SolidProgram.cs
+++ MyLibrary/MySOLID/SolidProgram.cs
@@ -9,10 +9,12 @@
 
         private void Run()
         {
-            var square = new Square(1);
+            var square = new Square(2);
             var circle = new Circle(2);
+            var hollowCircle = new HollowCircle(2, 1);
+            var cube = new Cube(2);
 
-            var calc = new AreaCalculator(square, circle);
+            var calc = new AreaCalculator(square, circle, hollowCircle, cube);
 
 
             var outputter = new AreaCalculatorOutputter(calc);
