7801bb8a8f201a075ea307fca8f195a5bc294db2
SSeelos <sebastian.seelos@gmx.de>
2021-08-11 16:29:01 +0200

    singleton + logger

---
+++ MyLibrary/MyDesignPrinciples/MySingleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace MyLibrary.MyDesignPrinciples
+{
+    public class MySingleton
+    {
+        private static MySingleton instance;
+        public static MySingleton Instance => GetInstance();
+
+        private MySingleton() { }
+
+        private static MySingleton GetInstance()
+        {
+            return instance == null ? new MySingleton() : instance;
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine(this.GetType().Name + ": " + MethodBase.GetCurrentMethod().Name);
+        }
+        public void DoSomethingElse()
+        {
+            Console.WriteLine(this.GetType().Name + ": " + MethodBase.GetCurrentMethod().Name);
+        }
+    }
+}
---
+++ MyLibrary/MyUtilities/MyLogger.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Reflection;
+
+namespace MyLibrary.MyUtilities
+{
+    public enum Hirarchy
+    {
+        Title,
+        Line
+    }
+    public interface ILogger
+    {
+        void ClassLog(Type classType, Hirarchy hirarchy = Hirarchy.Line);
+        void MethodLog(MethodBase method, Hirarchy hirarchy = Hirarchy.Line);
+        void ClassMethodLog(Type classType, MethodBase method, Hirarchy hirarchy = Hirarchy.Line);
+    }
+    public class MyConsoleLogger : ILogger
+    {
+        private static MyConsoleLogger instance;
+        public static MyConsoleLogger Instance => GetInstance();
+
+        private MyConsoleLogger() { }
+
+        private static MyConsoleLogger GetInstance()
+        {
+            //if (instance == null)
+            //    instance = new MyConsoleLogger();
+
+            //return instance;
+
+            return instance == null ? new MyConsoleLogger() : instance;
+        }
+        public void ClassLog(Type classType, Hirarchy hirarchy = Hirarchy.Line)
+        {
+            switch (hirarchy)
+            {
+                case Hirarchy.Title:
+                    Console.WriteLine();
+                    break;
+                case Hirarchy.Line:
+                    break;
+                default:
+                    break;
+
+            }
+
+            Console.WriteLine(classType.GetType().Name + " ");
+            Console.WriteLine();
+        }
+
+        public void ClassMethodLog(Type classType, MethodBase method, Hirarchy hirarchy = Hirarchy.Line)
+        {
+            switch (hirarchy)
+            {
+                case Hirarchy.Title:
+                    Console.WriteLine();
+                    break;
+                case Hirarchy.Line:
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(classType.GetType().Name + ": " + method.Name);
+            Console.WriteLine();
+        }
+
+        public void MethodLog(MethodBase method, Hirarchy hirarchy = Hirarchy.Line)
+        {
+            switch (hirarchy)
+            {
+                case Hirarchy.Title:
+                    Console.WriteLine();
+                    Console.WriteLine(method.Name + " ");
+                    Console.WriteLine();
+                    break;
+                case Hirarchy.Line:
+                    Console.WriteLine(method.Name + " ");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
