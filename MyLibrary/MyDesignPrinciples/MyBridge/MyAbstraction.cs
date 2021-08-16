0865e3409cbf975766fb340cd6be5a86cfc1bb39
SSeelos <sebastian.seelos@gmx.de>
2021-08-16 17:47:58 +0200

    bridge

---
+++ MyLibrary/MyDesignPrinciples/MyBridge/MyAbstraction.cs
@@ -0,0 +1,42 @@
+﻿using MyLibrary.MyUtilities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MyLibrary.MyDesignPrinciples.MyBridge
+{
+    public class MyAbstraction
+    {
+        protected IImplementation implementation;
+
+        public MyAbstraction(IImplementation implementation)
+        {
+            this.implementation = implementation;
+        }
+
+        public void FeatureA()
+        {
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+            implementation.MethodA();
+        }
+        public void FeatureB()
+        {
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+            implementation.MethodB();
+        }
+
+    }
+
+    public class MySubAbstraction : MyAbstraction
+    {
+        public MySubAbstraction(IImplementation implementation) : base(implementation)
+        {
+        }
+        public void SubFeature()
+        {
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+            implementation.MethodA();
+        }
+    }
+}
---
+++ MyLibrary/MyDesignPrinciples/MyBridge/MyImplementation.cs
@@ -0,0 +1,43 @@
+﻿using MyLibrary.MyUtilities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MyLibrary.MyDesignPrinciples.MyBridge
+{
+    public interface IImplementation
+    {
+        void MethodA();
+        string MethodB();
+    }
+    public class MyImplementationA : IImplementation
+    {
+        private string propertyImpA;
+        public void MethodA()
+        {
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+            
+        }
+
+        public string MethodB()
+        {
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+            return this.propertyImpA;
+        }
+    }
+    public class MyImplementationB : IImplementation
+    {
+        private string propertyImpB;
+        public void MethodA()
+        {
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+        }
+
+        public string MethodB()
+        {
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+            return this.propertyImpB;
+        }
+    }
+}
