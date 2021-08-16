3147f14ed836a07dfa32e35d8eb845cbb650dc7b
SSeelos <sebastian.seelos@gmx.de>
2021-08-16 17:48:48 +0200

    strategy

---
+++ MyLibrary/MyDesignPrinciples/MyStrategy/MyContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyLibrary.MyDesignPrinciples.MyStrategy
+{
+    public class MyContext
+    {
+        private IStrategy _strategy;
+        public IStrategy Strategy
+        {
+            get => this._strategy;
+            set => this._strategy = value;
+        }
+        public MyContext() { }
+        public MyContext(IStrategy strategy)
+        {
+            this.Strategy = strategy;
+        }
+
+        public void ExecuteStrategy(string data)
+        {
+            try
+            {
+                this._strategy.Execute(data);
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ MyLibrary/MyDesignPrinciples/MyStrategy/MyStrategies.cs
@@ -0,0 +1,29 @@
+﻿using MyLibrary.MyUtilities;
+using System;
+using System.Reflection;
+
+namespace MyLibrary.MyDesignPrinciples.MyStrategy
+{
+    public interface IStrategy
+    {
+        void Execute(string data);
+    }
+    public class MyStrategyA : IStrategy
+    {
+        public void Execute(string data)
+        {
+            data += " A";
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+            Console.WriteLine(data);
+        }
+    }
+    public class MyStrategyB : IStrategy
+    {
+        public void Execute(string data)
+        {
+            data += " B";
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+            Console.WriteLine(data);
+        }
+    }
+}
