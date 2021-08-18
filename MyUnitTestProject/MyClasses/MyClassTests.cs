5479ffaa16b756c29b54398d28095c5fdd68d946
SSeelos <sebastian.seelos@gmx.de>
2021-08-18 18:06:03 +0200

    class tests

---
+++ MyUnitTestProject/MyClasses/MyClassTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyConsoleAppProject.Tests
+{
+    [TestClass()]
+    public class MyClassTests
+    {
+        private MyClass _myClass;
+        private MyClass MyClass => _myClass != null ? _myClass : MyClassTest();
+
+        [TestMethod()]
+        public MyClass MyClassTest()
+        {
+            var myClass = new MyClass("value");
+
+            Assert.IsNotNull(myClass);
+
+            return myClass;
+        }
+
+        [TestMethod()]
+        public void MyMethodTest()
+        {
+            MyClass.myProperty = 2;
+
+            var myClass = MyClass;
+            myClass.myProperty += 2;
+
+
+
+            var result = MyClass.MyMethod();
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void MyInterfaceFunctionTest()
+        {
+            MyClass.MyInterfaceFunction();
+        }
+
+        [TestMethod()]
+        public void MyInterface2FunctionTest()
+        {
+            MyClass.MyInterfaceFunction();
+        }
+    }
+}
