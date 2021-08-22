3ae8f7285231a6021b2c0c9d56c61789cf8c55ef
SSeelos <sebastian.seelos@gmx.de>
2021-08-22 12:17:16 +0200

    adapter client

---
+++ MyLibrary/MyDesignPrinciples/Adapter/MyAdapterClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.MyDesignPrinciples.Adapter
+{
+    class MyAdapterClient
+    {
+        internal void Run()
+        {
+            var compatibleService = new MyCompatibleService("compatible");
+            var service = new MyIncompatibleService(5);
+            var adapter = new MyAdapter(service);
+
+            var receiver = new MyServiceReceiver();
+            receiver.UseService(compatibleService);
+            receiver.UseService(adapter);
+        }
+    }
+}
