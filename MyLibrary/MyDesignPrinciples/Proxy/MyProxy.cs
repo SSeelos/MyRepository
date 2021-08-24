465c330fef248227aacf63fdba6d0690df620508
SSeelos <sebastian.seelos@gmx.de>
2021-08-24 16:26:41 +0200

    my proxy

---
+++ MyLibrary/MyDesignPrinciples/Proxy/MyProxy.cs
@@ -0,0 +1,16 @@
+﻿using MyLibrary.MyUtilities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MyLibrary.MyDesignPrinciples.Proxy
+{
+    class MyProxy : IService
+    {
+        public void Operation()
+        {
+            MyConsoleLogger.Instance.ClassMethodLog(this.GetType(), MethodBase.GetCurrentMethod());
+        }
+    }
+}
---
+++ MyLibrary/MyDesignPrinciples/Proxy/MyProxyClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.MyDesignPrinciples.Proxy
+{
+    class MyProxyClient
+    {
+        public static void Run()
+        {
+            var proxy = new MyProxy();
+            var service = new MyService();
+
+            UseService(proxy);
+            UseService(service);
+        }
+        private static void UseService(IService service)
+        {
+            service.Operation();
+        }
+    }
+}
