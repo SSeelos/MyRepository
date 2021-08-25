c06c7347a3536e79dcf834b23bfb8431f9fa65b3
SSeelos <sebastian.seelos@gmx.de>
2021-08-25 09:21:43 +0200

    +

---
+++ MyLibrary/MyDesignPrinciples/MyTemplateMethod/MyTemplateClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.MyDesignPrinciples.MyTemplateMethod
+{
+    class MyTemplateClient
+    {
+        public static void Run()
+        {
+            var defaultClass = new DefaultClass();
+            defaultClass.TemplateMethod1();
+            defaultClass.TemplateMethod2();
+
+            var concreteCLass = new ConcreteClass();
+            concreteCLass.TemplateMethod1();
+        }
+    }
+}
