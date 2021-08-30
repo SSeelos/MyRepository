bc3d2155653f5422d4e0ecaf3eaf581872335fcd
SSeelos <sebastian.seelos@gmx.de>
2021-08-30 18:03:11 +0200

    todo

---
+++ MyLibrary/MyDesignPrinciples/Iterator (todo)/MyCollection.cs
@@ -0,0 +1,19 @@
+﻿namespace MyLibrary.MyDesignPrinciples.Iterator
+{
+    public interface ICollection
+    {
+        IIterator CreateIteratorA();
+        IIterator CreateIteratorB();
+    }
+    class MyCollectionA : ICollection
+    {
+        public IIterator CreateIteratorA()
+        {
+            return new MyIteratorA(this);
+        }
+        public IIterator CreateIteratorB()
+        {
+            return new MyIteratorB(this);
+        }
+    }
+}
---
+++ MyLibrary/MyDesignPrinciples/Iterator (todo)/MyIterator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyLibrary.MyDesignPrinciples.Iterator
+{
+    public interface IIterator
+    {
+        void GetNext();
+        bool HasMore();
+    }
+    public class MyIteratorA : IIterator
+    {
+        private ICollection collection;
+        private bool state;
+        public MyIteratorA(ICollection collection)
+        {
+            this.collection = collection;
+        }
+        public void GetNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasMore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MyIteratorB : IIterator
+    {
+        private ICollection collection;
+        private bool state;
+        public MyIteratorB(ICollection collection)
+        {
+            this.collection = collection;
+        }
+        public void GetNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasMore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
