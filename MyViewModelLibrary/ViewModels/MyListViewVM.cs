b8d48ca57e3aed11bad30450016f502c315fb04a
SSeelos <sebastian.seelos@gmx.de>
2021-09-20 12:07:16 +0200

    list view + binding + data manipulation

---
+++ MyViewModelLibrary/ViewModels/MyListViewVM.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MyViewModelLibrary.ViewModels
+{
+    public class MyListViewVM : MyObservableVMBase
+    {
+        public MyListViewVM()
+        {
+            MyItemDatas.Add(new MyItemData() { DataA = "A1", DataB = "B1" });
+            MyItemDatas.Add(new MyItemData() { DataA = "A2", DataB = "B2" });
+            UseDataCommand = new RelayCommand(UseData);
+        }
+        private int myVar;
+        public int MyProperty
+        {
+            get => myVar;
+            set => SetProperty(ref myVar, value);
+        }
+        private MyItemData _itemData;
+        public MyItemData ItemData
+        {
+            get => _itemData;
+            set => SetProperty(ref _itemData, value);
+        }
+
+        public ObservableCollection<MyItemData> MyItemDatas { get; } = new ObservableCollection<MyItemData>();
+
+        public ICommand UseDataCommand { get; }
+
+        private void UseData()
+        {
+            //new instance -> refresh display value (optional)
+            ItemData = new MyItemData()
+            {
+                DataA = ItemData.DataA += "+",
+                DataB = ItemData.DataB += "+",
+            };
+        }
+    }
+
+    public class MyItemData
+    {
+        public string DataA { get; set; }
+        public string DataB { get; set; }
+
+        public override string ToString()
+        {
+            return DataA + DataB;
+        }
+    }
+}
--- MyWPF/MVVM/MyMVVMToolkit/MyMVVMToolkitWindow.xaml.cs
+++ MyWPF/MVVM/MyMVVMToolkit/MyMVVMToolkitWindow.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MyWPF.MVVM.MyMVVMToolkit.Views;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MyWPF.MVVM.MyMVVMToolkit
 {
@@ -27,6 +18,12 @@ namespace MyWPF.MVVM.MyMVVMToolkit
         {
             new MyObservableView()
                 .Show();
+        }
+
+        private void ListView_Click(object sender, RoutedEventArgs e)
+        {
+            new MyListView()
+                .ShowDialog();
         }
     }
 }
