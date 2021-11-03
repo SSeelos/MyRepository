1ce56dcb5e55ee0632570f86261dfac1dea17cdc
SSeelos <sebastian.seelos@gmx.de>
2021-11-03 15:33:35 +0100

    Process program

---
+++ MyLibrary/MyProcess/MyProcessHandler.cs
@@ -0,0 +1,92 @@
+﻿using MyLibrary.MyUtilities;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace MyLibrary.MyProcess
+{
+    public interface IProcessHandler
+    {
+        void Init(string fileName);
+        void ReplaceProcess(string fileName);
+        void Kill();
+    }
+    class MyProcessHandler : IProcessHandler
+    {
+        public static int? MyProcessId = null;
+        public void Init(string fileName)
+        {
+            MyConsoleLogger.Instance.MethodLog(MethodBase.GetCurrentMethod(), Hirarchy.Title);
+
+            using (var myProcess = new Process())
+            {
+                myProcess.StartInfo.UseShellExecute = true;
+                myProcess.StartInfo.FileName = fileName;
+                myProcess.EnableRaisingEvents = true;
+                myProcess.Exited += new EventHandler(MyProcess_Exited);
+                myProcess.Start();
+
+                var machName = myProcess.MachineName;
+
+                var name = myProcess.ProcessName;
+
+                MyProcessId = myProcess.Id;
+
+            }
+        }
+        private void MyProcess_Exited(object sender, EventArgs e)
+        {
+            Console.WriteLine("exited");
+            Process.GetProcessById(MyProcessId.Value).Exited -= MyProcess_Exited;
+        }
+
+        public void ReplaceProcess(string fileName)
+        {
+            MyConsoleLogger.Instance.MethodLog(MethodBase.GetCurrentMethod());
+
+            this.Kill();
+
+            this.StartProcess(fileName);
+
+        }
+        private void StartProcess(string fileName)
+        {
+            MyConsoleLogger.Instance.MethodLog(MethodBase.GetCurrentMethod());
+
+            if (!File.Exists(fileName))
+            {
+                return;
+            }
+
+            using (var process = new Process())
+            {
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.FileName = fileName;
+                process.Start();
+
+                MyProcessId = process.Id;
+            }
+
+        }
+        public void Kill()
+        {
+            MyConsoleLogger.Instance.MethodLog(MethodBase.GetCurrentMethod());
+
+            if (MyProcessId == null)
+                return;
+
+            try
+            {
+                var process = Process.GetProcessById(MyProcessId.Value);
+
+                process?.Kill();
+
+            }
+            catch
+            {
+                //nothing to kill, all good?
+            }
+        }
+    }
+}
---
+++ MyLibrary/MyProcess/_MyProcessProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyLibrary.MyProcess
+{
+    public class MyProcessProgram : IProgram
+    {
+        IProcessHandler processHandler = new MyProcessHandler();
+        //private Process myProcess;
+        public void Run()
+        {
+            processHandler.Init(@"C:\Temp\12254_angebot_NR390.ifc");
+
+            Console.ReadLine();
+            processHandler.ReplaceProcess(@"C:\Temp\1825786_angebot_NR1351.ifc");
+
+            Console.ReadLine();
+            processHandler.Kill();
+        }
+    }
+}
