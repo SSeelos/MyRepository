f7fc0b5c4cfcbbb7db4f86384550cc5854e49478
SSeelos <sebastian.seelos@gmx.de>
2021-11-15 10:26:09 +0100

    unitsnet

---
+++ MyLibrary/MyUnitValues/MyUnits/UnitValueT/UnitValueT.cs
@@ -0,0 +1,50 @@
+﻿namespace MyLibrary
+{
+
+    public abstract class UnitValueT<T>
+    {
+        public UnitValueT(T v)
+        {
+            this._DisplayValue = v;
+            this._BaseValue = this.toBaseUnit(v);
+        }
+        public UnitValueT(UnitValueT<T> v)
+        {
+            this._DisplayValue = this.fromBaseUnit(v.BaseValue);
+            this.BaseValue = v.BaseValue;
+        }
+
+
+        private T _DisplayValue;
+        private T _BaseValue;
+        public T DisplayValue
+        {
+            get => this._DisplayValue;
+            set
+            {
+                this._DisplayValue = value;
+                this._BaseValue = this.toBaseUnit(value);
+            }
+        }
+        public T BaseValue
+        {
+            get => this._BaseValue;
+            set
+            {
+                this._DisplayValue = fromBaseUnit(value);
+                this._BaseValue = value;
+            }
+        }
+
+        public abstract T fromBaseUnit(T v);
+        public abstract T toBaseUnit(T v);
+
+
+    }
+
+
+
+
+
+
+}
---
+++ MyLibrary/MyUnitValues/UnitsNet/_UnitsNetProgramm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitsNet;
+
+namespace MyLibrary
+{
+    public class UnitsNetProgram : IProgram
+    {
+        public void Run()
+        {
+            var qu = new QuantityValue();
+            qu = 1000;
+            Length lengthA = Length.From(qu, UnitsNet.Units.LengthUnit.Millimeter);
+            Length lengthB = Length.From(100, UnitsNet.Units.LengthUnit.Centimeter);
+
+            Length result = lengthB+lengthA;
+            var conv = result.Meters;
+        }
+    }
+}
