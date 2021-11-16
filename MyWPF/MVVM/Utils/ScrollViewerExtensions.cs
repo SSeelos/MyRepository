bd933e5405eff67d6855d0ad5265c09adafce9dd
SSeelos <sebastian.seelos@gmx.de>
2021-11-16 09:12:49 +0100

    listview data collection

---
+++ MyWPF/MVVM/Utils/ScrollViewerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MyWPF.MVVM.Utils
+{
+    public static class ScrollViewerExt
+    {
+        public static void EnableMouseWheelScroll(this ScrollViewer scrollViewer, MouseWheelEventArgs e)
+        {
+            scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+
+            if (e.Delta > 0)
+            {
+                scrollViewer.LineUp();
+            }
+            else
+            {
+                scrollViewer.LineDown();
+            }
+            e.Handled = true;
+        }
+    }
+}
