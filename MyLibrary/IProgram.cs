ae5a33b3465c5b97099fdfacd88228a798960ef3
SSeelos <sebastian.seelos@gmx.de>
2021-11-17 12:13:23 +0100

    program runner

--- MyLibrary/IProgram.cs
+++ MyLibrary/IProgram.cs
@@ -7,4 +7,34 @@ namespace MyLibrary
         void Run();
         void Close() => Console.Clear();
     }
+
+    public class ProgramRunner
+    {
+        private IProgram _currentProgram;
+        public IProgram CurrentProgram
+        {
+            get { return _currentProgram; }
+            set
+            {
+                if (_currentProgram != null)
+                    _currentProgram.Close();
+
+                _currentProgram = value;
+            }
+        }
+        public void RunProgram(IProgram program)
+        {
+            CurrentProgram = program;
+
+            try
+            {
+                program.Run();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
 }
