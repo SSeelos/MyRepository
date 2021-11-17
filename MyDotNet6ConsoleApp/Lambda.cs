9643af04ac42cab15b9e503b1b5d527c95d35d79
SSeelos <sebastian.seelos@gmx.de>
2021-11-17 12:07:42 +0100

    new lambda stuff

---
+++ MyDotNet6ConsoleApp/Lambda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDotNet6ConsoleApp
+{
+    internal class Lambda
+    {
+        public Lambda()
+        {
+            Func<string, int>? parseFunc = (string s) => int.Parse(s);
+
+            //creates new anonymous delegate
+            var parse = (ref string s) => int.Parse(s);
+
+            //infer expression from agument types
+            Expression parseExp = (string s) => int.Parse(s);
+
+        }
+    }
+}
