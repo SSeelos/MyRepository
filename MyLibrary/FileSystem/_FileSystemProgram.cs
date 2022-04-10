2b45ff5d856acb66b87b1e8cd4606c4796070949
SSeelos <sebastian.seelos@gmx.de>
2022-04-10 15:39:59 +0200

    filesystem

---
+++ MyLibrary/FileSystem/_FileSystemProgram.cs
@@ -0,0 +1,14 @@
+﻿namespace MyLibrary_DotNETstd_2_1
+{
+    public class FileSystemProgram : IProgram
+    {
+        public void Run()
+        {
+            var examples = new Examples(
+                new GetDirectoriesEx(),
+                new GetFilesEx());
+
+            examples.Execute();
+        }
+    }
+}
