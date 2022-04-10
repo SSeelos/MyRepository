d041554541b2fdeb82642b38fbf7c368074dfe32
SSeelos <sebastian.seelos@gmx.de>
2022-04-10 15:40:09 +0200

    regex

---
+++ MyLibrary/MyRegex/_MyRegexProgram.cs
@@ -0,0 +1,13 @@
+﻿namespace MyLibrary_DotNETstd_2_1.MyRegex
+{
+    public class MyRegexProgram : IProgram
+    {
+        public void Run()
+        {
+            var examples = new Examples(
+                new RegexEx());
+
+            examples.Execute();
+        }
+    }
+}
