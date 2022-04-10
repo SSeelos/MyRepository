d9bc80ce9a1a7a7a8eab4f7aa772602eba949705
SSeelos <sebastian.seelos@gmx.de>
2022-04-10 15:39:36 +0200

    installer

---
+++ MyInstallerLib/MyInstaller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace MyInstallerLib
+{
+    [RunInstaller(true)]
+    public partial class MyInstaller : Installer
+    {
+        public MyInstaller()
+        {
+            InitializeComponent();
+        }
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            //var version = Context.Parameters["version"];
+        }
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+        }
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+    }
+}
