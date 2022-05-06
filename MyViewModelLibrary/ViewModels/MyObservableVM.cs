f61eb5a45e7ce3f6ca0f23a3d63b9a11ab065324
SSeelos <sebastian.seelos@gmx.de>
2022-05-06 17:38:53 +0200

    wpf data vailidation/errors

--- MyViewModelLibrary/ViewModels/MyObservableVM.cs
+++ MyViewModelLibrary/ViewModels/MyObservableVM.cs
@@ -1,18 +1,30 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace MyViewModelLibrary.ViewModels
 {
-    public partial class MyObservableVM : _MyVM
+    public partial class MyObservableVM : _MyVM, INotifyDataErrorInfo
     {
         public MyObservableVM()
         {
             MyRelayCommand = new RelayCommand(MyAction);
             MyRelayCommandLambda = new RelayCommand(() => MyProperty = "RelayCmdLambda");
 
+            Func<bool> canExecuteCheck = () => MyPropertyNotify != "";
+            MyRelayCommandCanExecute = new RelayCommand(() => MyPropertyNotify += "can execute", canExecuteCheck);
+            //MyRelayCommandCanExecute.NotifyCanExecuteChanged();
+            MyRelayCommandCanExecute.CanExecute(false);
+
             ResetCommand = new RelayCommand(Reset);
             MyObservablePropertyCommand = new RelayCommand(() => MyObservableProperty = "MyObservableProperty");
+
+
         }
 
 
@@ -21,6 +33,7 @@ namespace MyViewModelLibrary.ViewModels
         public ICommand MyRelayCommandLambda { get; }
         public ICommand MyObservablePropertyCommand { get; }
         public ICommand MyVMCommand => new MyCommandVM(this);
+        public RelayCommand MyRelayCommandCanExecute { get; }
 
         private const string defaultValue = "default";
         private string _myProperty = defaultValue;
@@ -32,6 +45,65 @@ namespace MyViewModelLibrary.ViewModels
 
         [ObservableProperty]
         string myObservableProperty = defaultValue;
+
+        private string _myPropertyNotify;
+
+
+        public string MyPropertyNotify
+        {
+            get => _myPropertyNotify;
+            set
+            {
+                SetProperty(ref _myPropertyNotify, value);
+                MyRelayCommandCanExecute.NotifyCanExecuteChanged();
+            }
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        private Dictionary<string, List<string>> _propertyErrors = new Dictionary<string, List<string>>();
+        public bool HasErrors => _propertyErrors.Any();
+        private string _myPropertyError;
+        public string MyPropertyError
+        {
+            get => _myPropertyError;
+            set
+            {
+                RemoveError(nameof(MyPropertyError));
+
+                if (value == "")
+                {
+                    AddError(nameof(MyPropertyError), "cant be empty");
+                    OnErrorsChanged(nameof(MyPropertyError));
+                }
+                else
+                {
+                    SetProperty(ref _myPropertyError, value);
+                }
+            }
+        }
+
+        private void AddError(string propertyName, string errormessage)
+        {
+            if (!_propertyErrors.ContainsKey(propertyName))
+            {
+                _propertyErrors.Add(propertyName, new List<string>());
+            }
+            _propertyErrors[propertyName].Add(errormessage);
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void RemoveError(string propertyName)
+        {
+            _propertyErrors.Remove(propertyName);
+        }
+
+        public MyObservableValidator MyObservableValidator { get; } = new MyObservableValidator() { MyValidatorProperty = defaultValue };
+
+
         private void MyAction()
         {
             MyProperty = "RelayCommand";
@@ -40,6 +112,13 @@ namespace MyViewModelLibrary.ViewModels
         {
             MyProperty = defaultValue;
             MyObservableProperty = defaultValue;
+            MyPropertyNotify = "";
+            MyObservableValidator.MyValidatorProperty = defaultValue;
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return _propertyErrors.GetValueOrDefault(propertyName, new List<string>());
         }
     }
 }
