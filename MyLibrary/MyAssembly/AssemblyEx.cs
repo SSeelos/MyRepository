985db585ac0e6c17927b86b99fa2ff4299269d08
SSeelos <sebastian.seelos@gmx.de>
2022-06-22 11:58:55 +0200

    assembly ex

---
+++ MyLibrary/MyAssembly/AssemblyEx.cs
@@ -0,0 +1,23 @@
+﻿using MyLibrary_DotNETstd_2_1.BetterOOP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyLibrary_DotNETstd_2_1.MyAssembly
+{
+    internal class AssemblyEx : IExample
+    {
+        public void Execute()
+        {
+            IEnumerable<Type> T =
+                from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+                from assemblyTypes in domainAssembly.GetTypes()
+                where typeof(_LandVehicle).IsAssignableFrom(assemblyTypes)
+                select assemblyTypes;
+
+            IEnumerable<Type> TT = typeof(_LandVehicle).Assembly
+                .GetTypes()
+                .Where(t => typeof(_LandVehicle).IsAssignableFrom(t));
+        }
+    }
+}
