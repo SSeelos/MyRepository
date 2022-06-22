b05ae5eb7a165962893724c665c2282ba5dfc7f2
SSeelos <sebastian.seelos@gmx.de>
2022-06-22 11:57:43 +0200

    custom validator

---
+++ MyViewModelLibrary/ViewModels/Attributes/MyCustomValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyViewModelLibrary.ViewModels
+{
+    public class MyValidationAttribute : ValidationAttribute
+    {
+        public string MyProperty { get; }
+        public string Exclude { get; }
+        public MyValidationAttribute(string myProperty, string exclude)
+        {
+            MyProperty = myProperty;
+            Exclude = exclude;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            object instance = validationContext.ObjectInstance;
+            object otherValue = instance.GetType().GetProperty(MyProperty).GetValue(instance);
+
+            if (value == otherValue)
+                return new ValidationResult("input excluded");
+
+            return ValidationResult.Success;
+        }
+    }
+}
