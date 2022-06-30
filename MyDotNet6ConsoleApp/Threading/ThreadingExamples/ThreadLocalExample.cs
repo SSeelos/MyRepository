93e6c9be8e27c0439ec82a6dbbbf70803342df30
SSeelos <sebastian.seelos@gmx.de>
2022-06-30 16:38:18 +0200

    parallel threading

---
+++ MyDotNet6ConsoleApp/Threading/ThreadingExamples/ThreadLocalExample.cs
@@ -0,0 +1,28 @@
+﻿namespace MyDotNet6ConsoleApp.Threading
+{
+    internal class ThreadLocalExample : IThreadingExample
+    {
+        public void Execute()
+        {
+            ThreadLocal<string> threadName = new ThreadLocal<string>(
+                () =>
+                {
+                    return $"Curren Thread: {Thread.CurrentThread.ManagedThreadId}";
+                }
+                );
+
+            Action action = () =>
+            {
+                bool repeat = threadName.IsValueCreated;
+                WriteLine($"{threadName.Value} repeat:{repeat}");
+            };
+
+            Parallel.Invoke(action, action, action, action, action, action, action, action, action, action,
+                action, action, action, action, action, action, action, action, action, action,
+                action, action, action, action, action, action, action, action, action, action,
+                action, action, action, action, action, action, action, action, action, action);
+
+            threadName.Dispose();
+        }
+    }
+}
