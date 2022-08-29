90874e022e6b5948e22552361eba6a5409e5e0c3
SSeelos <sebastian.seelos@gmx.de>
2022-08-29 16:37:02 +0200

    minor stuff

--- MyLibrary/MyDesignPrinciples/Creational/AbstractFactory/MyAbstractFactory.cs
+++ MyLibrary/MyDesignPrinciples/Creational/AbstractFactory/MyAbstractFactory.cs
@@ -45,14 +45,14 @@ namespace MyLibrary_DotNETstd_2_1.MyDesignPrinciples.AbstractFactory
     {
         public void doSomething()
         {
-            Console.WriteLine(this.GetType().Name + ": " + MethodBase.GetCurrentMethod().Name);
+            Console.WriteLine($"{this.GetType().Name} : {MethodBase.GetCurrentMethod().Name}");
         }
     }
     public class ProductB1 : IProduct
     {
         public void doSomething()
         {
-            Console.WriteLine(this.GetType().Name + ": " + MethodBase.GetCurrentMethod().Name);
+            Console.WriteLine($"{this.GetType().Name} : {MethodBase.GetCurrentMethod().Name}");
         }
     }
 
