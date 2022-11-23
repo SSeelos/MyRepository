9fd38cfcf76a23312bb86f249a0184fd30cacfb2
SSeelos <sebastian.seelos@gmx.de>
2022-11-23 21:31:36 +0100

    inherted fixture

---
+++ MyXUnitTestProject/XUnit/SharedContext/MyClassFixtureC.cs
@@ -0,0 +1,31 @@
+﻿using MyXUnitTestProject.XUnit.SharedContext;
+
+namespace MyXUnitTestProject.XUnit
+{
+    /// <summary>
+    /// For each test, it will create a new instance of MyClassFixtureB,
+    /// and pass the shared instance of MyFixtureA and MyFixtureB to the constructor.
+    /// </summary>
+    public class MyClassFixtureC : IClassFixture<MyFixtureC>
+    {
+        MyFixtureC myFixture;
+        public MyClassFixtureC(MyFixtureC myFixture)
+        {
+            this.myFixture = myFixture;
+        }
+
+        [Fact]
+        public void MyFactA()
+        {
+            var result = myFixture.Data
+                + myFixture.DataC
+                + nameof(MyFactA);
+
+            Assert.Equal(
+                nameof(MyFixtureC) + nameof(myFixture.Data) +
+                nameof(MyFixtureC) + nameof(myFixture.DataC) +
+                nameof(MyFactA),
+                result);
+        }
+    }
+}
--- MyXUnitTestProject/XUnit/SharedContext/MyFixture.cs
+++ MyXUnitTestProject/XUnit/SharedContext/MyFixture.cs
@@ -1,32 +1,47 @@
-﻿namespace MyXUnitTestProject.XUnit.SharedContext
+﻿namespace MyXUnitTestProject.XUnit.SharedContext;
+
+/// <summary>
+/// Just before the first test in MyClassFixtureTests is run,
+/// xUnit.net will create an instance of MyFixtureA
+/// (fixtures cannot take dependencies on other fixtures)
+/// </summary>
+public class MyFixtureA : IDisposable
 {
-    /// <summary>
-    /// Just before the first test in MyClassFixtureTests is run,
-    /// xUnit.net will create an instance of MyFixtureA
-    /// </summary>
-    public class MyFixtureA : IDisposable
+    public string? Data { get; private set; }
+    public MyFixtureA()
     {
-        public string? Data { get; private set; }
-        public MyFixtureA()
-        {
-            Data = nameof(MyFixtureA) + nameof(Data);
-        }
-        public void Dispose()
-        {
-            Data = null;
-        }
+        Data = this.GetType().Name + nameof(Data);
     }
-    /// </summary>
-    public class MyFixtureB : IDisposable
+    public void Dispose()
     {
-        public string? Data { get; private set; }
-        public MyFixtureB()
-        {
-            Data = nameof(MyFixtureB) + nameof(Data);
-        }
-        public void Dispose()
-        {
-            Data = null;
-        }
+        Data = null;
+    }
+}
+
+public class MyFixtureB : IDisposable
+{
+    public string? Data { get; private set; }
+    public MyFixtureB()
+    {
+        Data = this.GetType().Name + nameof(Data);
+    }
+    public void Dispose()
+    {
+        Data = null;
+    }
+}
+
+//inheritance works (is it best practice tho?)
+public class MyFixtureC : MyFixtureA, IDisposable
+{
+    public string? DataC { get; private set; }
+    public MyFixtureC()
+    {
+        DataC = this.GetType().Name + nameof(DataC);
+    }
+    public new void Dispose()
+    {
+        base.Dispose();
+        DataC = null;
     }
 }
