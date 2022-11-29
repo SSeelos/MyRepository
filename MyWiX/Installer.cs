270d8ce79e5f0de6f8b8e77aad08683e3c4e4008
SSeelos <sebastian.seelos@gmx.de>
2022-11-29 09:05:23 +0100

    wix

---
+++ MyWiX/Installer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System.Windows.Forms;
+using WixSharp;
+
+namespace MyWiX
+{
+    public static class Installer
+    {
+        static string productName = "MyProduct";
+        public static void Main(string[] args)
+        {
+            var dir = new Dir($@"ProgramFiles\MyCompany\{productName}",
+                new DirFiles(@"Release\Bin\*.*"));
+
+            var project = new Project(productName, dir);
+
+            project.BuildMsi();
+        }
+    }
+
+    public class CustomAcitons
+    {
+        [CustomAction]
+        public static ActionResult MyAction(Session session)
+        {
+            MessageBox.Show($"{nameof(MyAction)}()");
+            session.Log($"session: {nameof(MyAction)}()");
+
+            return ActionResult.Success;
+        }
+    }
+}
